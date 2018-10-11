a022843a765e6cd95bcb82c0164df7ecf14d6a6f
Puneeth <puneeth.nanjundaswamy@gmail.com>
2018-10-11 12:39:06 +0200

    Add enable flag (#7)

--- triggers/cloudwatch_event_trigger/main.tf
+++ triggers/cloudwatch_event_trigger/main.tf
@@ -10,6 +10,7 @@ variable "event_config" {
 variable "lambda_function_arn" {}
 
 resource "aws_cloudwatch_event_rule" "event_rule" {
+  count       = "${var.enable}"
   name        = "${lookup(var.event_config, "name")}"
   description = "${lookup(var.event_config, "description")}"
 
@@ -17,11 +18,13 @@ resource "aws_cloudwatch_event_rule" "event_rule" {
 }
 
 resource "aws_cloudwatch_event_target" "lambda" {
-  rule = "${aws_cloudwatch_event_rule.event_rule.name}"
-  arn  = "${var.lambda_function_arn}"
+  count = "${var.enable}"
+  rule  = "${aws_cloudwatch_event_rule.event_rule.name}"
+  arn   = "${var.lambda_function_arn}"
 }
 
 resource "aws_lambda_permission" "invoke-from-events" {
+  count         = "${var.enable}"
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
   function_name = "${var.lambda_function_arn}"
