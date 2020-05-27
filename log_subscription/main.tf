46ea2132156fcd82a2ec1b7355e64b431bd11e5c
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-05-27 14:06:55 +0200

    nits

--- log_subscription/main.tf
+++ log_subscription/main.tf
@@ -1,19 +1,32 @@
 variable "enable" {
-  default = 0
+  default     = false
+  type        = string
+  description = "Enable this module"
 }
 
-variable "lambda_name" {}
+variable "lambda_name" {
+  type        = string
+  description = "Lambda name"
+}
 
-variable "log_group_name" {}
+variable "log_group_name" {
+  type        = string
+  description = "Lambda log group name"
+}
 
 variable "cloudwatch_log_subscription" {
-  type = "map"
+  type = object({
+    cloudwatch_log_subscription : string
+    destination_arn : string
+  })
+  description = "Lambda CloudWatch log subscription"
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "lambda_cloudwatch_subscription" {
-  count           = "${var.enable}"
-  name            = "${var.lambda_name}"
-  log_group_name  = "${var.log_group_name}"
-  filter_pattern  = "${lookup(var.cloudwatch_log_subscription, "filter_pattern", "")}"
-  destination_arn = "${lookup(var.cloudwatch_log_subscription, "destination_arn", "")}"
+  count           = var.enable ? 1 : 0
+  name            = var.lambda_name
+  log_group_name  = var.log_group_name
+  filter_pattern  = var.cloudwatch_log_subscription.filter_pattern
+  destination_arn = var.cloudwatch_log_subscription.destination_arn
 }
+
