106b9f234f1db2348a73d08794987f51c37508e1
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-05-27 15:46:35 +0200

    nits

--- triggers/cognito_idp/main.tf
+++ triggers/cognito_idp/main.tf
@@ -1,13 +1,19 @@
 variable "enable" {
-  default = 0
+  default     = false
+  type        = bool
+  description = "Enable module"
 }
 
-variable "lambda_function_arn" {}
+variable "lambda_function_arn" {
+  type        = string
+  description = "Lambda function arn"
+}
 
 resource "aws_lambda_permission" "allow_invocation_from_cognito_idp" {
-  count         = "${var.enable}"
+  count         = var.enable ? 1 : 0
   statement_id  = "AllowExecutionFromCognito"
   action        = "lambda:InvokeFunction"
-  function_name = "${var.lambda_function_arn}"
+  function_name = var.lambda_function_arn
   principal     = "cognito-idp.amazonaws.com"
 }
+
